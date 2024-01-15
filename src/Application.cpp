e5e22c364dabdda57fde9f191dabcd23528157dd
Benjamín Boeri <ben9652@gmail.com>
2024-01-14 21:58:15 -0300

    Agrego una carpeta de recursos en donde guardaré los códigos de los shaders de OpenGL en unos archivos .shader | Optimizo el uso de memoria al dibujar un triángulo adyacente

--- src/Application.cpp
+++ src/Application.cpp
@@ -1,6 +1,104 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
 #include "GL/glew.h"
 #include "GLFW/glfw3.h"
+
+struct ShaderProgramSource
+{
+    std::string VertexSource;
+    std::string FragmentSource;
+};
+
+static ShaderProgramSource ParseShader(const std::string& filepath)
+{
+    std::ifstream stream(filepath);
+
+    enum class ShaderType
+    {
+        NONE = -1, VERTEX = 0, FRAGMENT = 1
+    };
+
+    std::string line;
+    std::stringstream ss[2];
+    ShaderType type = ShaderType::NONE;
+    while (getline(stream, line))
+    {
+        if (line.find("#shader") != std::string::npos)
+        {
+            if (line.find("vertex") != std::string::npos)
+                type = ShaderType::VERTEX;
+            else if (line.find("fragment") != std::string::npos)
+                type = ShaderType::FRAGMENT;
+        }
+        else
+        {
+            ss[(int)type] << line << '\n';
+        }
+    }
+
+    return { ss[0].str(), ss[1].str() };
+}
+
+static unsigned int CompileShader(unsigned int type, const std::string& source)
+{
+    unsigned int id = glCreateShader(type);
+    const char* src = source.c_str();
+
+    /*
+        - shader: id que especifica el gestor del objeto del shader cuyo código fuente será reeplazado
+        - count: el número de códigos fuente que estamos especificando
+        - string: especifica el array de punteros a strigns conteniendo el código fuente a ser cargado al shader
+        - lenght: especifica una longitud para el array de strings. Si se pone un NULL aquí se asume a cada string como que termina con un caracter nulo
+    */
+    glShaderSource(id, 1, &src, nullptr);
+    glCompileShader(id);
+
+    // Error handling
+    int result;
+    glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+    if (result == GL_FALSE)
+    {
+        int length;
+        glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+        char* message = (char*)alloca(length * sizeof(char));
+        glGetShaderInfoLog(id, length, &length, message);
+        std::cout << "Failed to compile " <<
+            (type == GL_VERTEX_SHADER ? "vertex" : "fragment") << " shader!" <<
+            std::endl;
+        std::cout << message << std::endl;
+        glDeleteShader(id);
+        return 0;
+    }
+
+    return id;
+}
+
+/// <summary>
+/// El propósito de esta función es proveer a OpenGL el código fuente del shader real,
+/// y queremos que OpenGL compile ese programa y enlace los dos juntos en un único
+/// programa que sería el shader completo.
+/// </summary>
+/// <param name="vertexShader"></param>
+/// <param name="fragmentShader"></param>
+/// <returns>Algún identificador único para ese shader creado para que podamos luego asociarlo y usarlo</returns>
+static unsigned int CreateShader(const std::string& vertexShader, const std::string& fragmentShader)
+{
+    unsigned int program = glCreateProgram();
+    unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader);
+    unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, fragmentShader);
+
+    glAttachShader(program, vs);
+    glAttachShader(program, fs);
+    glLinkProgram(program);
+    glValidateProgram(program);
+
+    glDeleteShader(vs);
+    glDeleteShader(fs);
+
+    return program;
+}
 
 int main(void)
 {
@@ -27,17 +125,30 @@ int main(void)
 
     std::cout << glGetString(GL_VERSION) << std::endl;
 
-    float positions[6] = {
+    float positions[] = {
         -0.5f, -0.5f,
-         0.0f,  0.5f,
-         0.5f, -0.5f
+        -0.5f,  0.5f,
+         0.5f, -0.5f,
+         0.5f,  0.5f
+    };
+
+    // Los primeros tres índices corresponden a los 3 vértices del primer triángulo
+    // Los segundos tres índices corresponden a los 3 vértices del segundo triángulo
+    unsigned int indices[] = {
+        0, 1, 2,
+        1, 2, 3
     };
 
     /* Creo un buffer para almacenar vértices */
     unsigned int vertexBuffer;
     glGenBuffers(1, &vertexBuffer);
     glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
-    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, 6 * 2 * sizeof(float), positions, GL_STATIC_DRAW);
+
+    unsigned int index_buffer;
+    glGenBuffers(1, &index_buffer);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, index_buffer);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), indices, GL_STATIC_DRAW);
 
     /*
         Describiré los atributos en orden:
@@ -76,14 +187,19 @@ int main(void)
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
     glEnableVertexAttribArray(0);
 
+    ShaderProgramSource shaders = ParseShader("res/shaders/Basic.shader");
+
+    unsigned int shader = CreateShader(shaders.VertexSource, shaders.FragmentSource);
+    glUseProgram(shader);
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
-        /* Dibujo los triángulos. El tercer parámetro cuenta realmente los vértices, es decir, los pares (x,y) de cada vértice. */
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        /* Dibujo los triángulos. El segundo parámetro cuenta realmente los vértices, es decir, los pares (x,y) de cada vértice. */
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
@@ -91,6 +207,8 @@ int main(void)
         /* Poll for and process events */
         glfwPollEvents();
     }
+
+    glDeleteProgram(shader);
 
     glfwTerminate();
     return 0;
